677d02390f68c8895f8e93f981c3d1a000d1e639
Aksana Dzeviantseichyk <a.dzeviantseichyk@gmail.com>
2023-04-17 13:07:07 +0200

    added solution for part 1

---
+++ Task_4/Part_1/Triangle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4.Part_1
+{
+    public class Triangle : Shape
+    {
+        private readonly double _lengthAB;
+        private readonly double _lengthBC;
+        private readonly double _lengthAC;
+        private readonly double _semiperimeter; 
+
+        public Triangle(double lengthAB, double lengthBC, double lengthAC)
+        {
+            _lengthAB = lengthAB;
+            _lengthBC = lengthBC;
+            _lengthAC = lengthAC;
+            _semiperimeter = (lengthAB + lengthBC + lengthAC) / 2;
+        }
+        public bool IsExists()
+        {
+            if (_lengthAB + _lengthBC > _lengthAC && _lengthAB + _lengthBC > _lengthAC && _lengthBC + _lengthAC > _lengthAB)
+                return true;
+            return false;
+        }
+
+        public override double GetArea() => 
+            Math.Sqrt(_semiperimeter*(_semiperimeter-_lengthAB)*(_semiperimeter-_lengthBC)*(_semiperimeter-_lengthAC));
+
+        public override double GetPerimeter() =>_lengthAB + _lengthBC + _lengthAC;
+        
+    }
+}
--- Task_4/Program.cs
+++ Task_4/Program.cs
@@ -1,2 +1,109 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+using Task_4.Part_1;
+
+namespace Task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String answer;
+            do
+            {
+                Console.WriteLine("Choose:");
+                Console.WriteLine("1 - Shapes");
+                Console.WriteLine("2 - Animals");
+                Console.WriteLine("3 - Fibonacci numbers using a loop");
+                Console.WriteLine("4 - Fibonacci numbers using recursion");
+                Console.WriteLine("5 - Polynomial");
+                Console.WriteLine("6 - Lightstring");
+                Console.WriteLine("Enter the number of the operation you want to execute:");
+
+                var numberOfOperation = Console.ReadLine().Trim();
+                switch (numberOfOperation)
+                {
+                    case "1":
+                        WorkWithShapes();
+                        break;
+                    case "2":
+                        
+                        break;
+                    case "3":
+                        
+                        break;
+                    case "4":
+                        
+                        break;
+                    case "5":
+
+                        break;
+                    case "6":
+
+                        break;
+                    default:
+                        Console.WriteLine("Unknown operation");
+                        break;
+                }
+
+
+                Console.WriteLine("Do you want to repeat (YES/NO)? If YES enter Y:");
+                answer = Console.ReadLine().ToUpper();
+
+            } while (answer == "Y");
+        }
+
+        #region case1
+        public static void WorkWithShapes()
+        {
+            Console.WriteLine("Choose a shape:");
+            Console.WriteLine("1 - Circle");
+            Console.WriteLine("2 - Square");
+            Console.WriteLine("3 - Triangle");
+            Console.WriteLine("Enter the number of the shape:");
+
+            var numberOfShape = Console.ReadLine().Trim();
+            try
+            {
+                switch (numberOfShape)
+                {
+                    case "1":
+                        Console.WriteLine("Enter the radius of the circle:");
+                        var radius = Double.Parse(Console.ReadLine());
+                        Circle circle = new Circle(radius);
+                        PrintShape(circle);
+                        break;
+                    case "2":
+                        Console.WriteLine("Enter the side length of the square:");
+                        var sideLength = Double.Parse(Console.ReadLine());
+                        Square square = new Square(sideLength);
+                        PrintShape(square);
+                        break;
+                    case "3":
+                        Console.WriteLine("Enter the AB length of the triangle:");
+                        var lengthAB = Double.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter the BC length of the triangle:");
+                        var lengthBC = Double.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter the AC length of the triangle:");
+                        var lengthAC = Double.Parse(Console.ReadLine());
+                        Triangle triangle = new Triangle(lengthAB,lengthBC, lengthAC);
+                        if (triangle.IsExists())
+                            PrintShape(triangle);
+                        else Console.WriteLine("This triangle IS NOT Exists ");
+                        break;
+                    default:
+                        Console.WriteLine("Unknown shape");
+                        break;
+                }
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+
+        }
+        public static void PrintShape(Shape shape)
+        {
+            Console.WriteLine($"Perimeter: {Math.Round(shape.GetPerimeter(),3)}\nArea: {Math.Round(shape.GetArea(),3)}");
+        }
+        #endregion
+    }
+
+}
